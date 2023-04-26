b88b2547cdfe48ff3260a558fccf1a7b3ca36322
Kushank Verma <kushankverma934@gmail.com>
2023-04-26 18:45:02 +0530

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCyMnV-CHz1gM7R1swTEg11l2NxJATAC8E",
+  authDomain: "mypro-20d35.firebaseapp.com",
+  projectId: "mypro-20d35",
+  storageBucket: "mypro-20d35.appspot.com",
+  messagingSenderId: "852545340742",
+  appId: "1:852545340742:web:4104d66e316db98b1d1147"
+};
+
+export default firebaseConfig;
